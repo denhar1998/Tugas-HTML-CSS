975b1a7aac04ccf57a012f7e2f04e36a770134da
denhar1998 <dennyhartanto19@gmail.com>
2024-01-20 12:49:44 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: { poppins: ["Poppins"] },
+      colors: { warna1: "#23155B", warna2: "#8053FF" },
+    },
+  },
+  plugins: [],
+};
